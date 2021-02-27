1aede5ee22c03ff9f6fb83b44341041a46a523c9
Maciej Matuszewski <maciej.matuszewski@me.com>
2021-02-27 18:51:43 +0000

    Update HomeViewController.swift

--- SpaceX Tracker/Modules/Home/HomeViewController.swift
+++ SpaceX Tracker/Modules/Home/HomeViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 import Style
+import SafariServices
 
 class HomeViewController: UIViewController {
     private let interactor: HomeInteractor
+    private let imageDownloader = ImageDownloader()
 
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
@@ -45,6 +47,7 @@ class HomeViewController: UIViewController {
         layoutComponents()
         interactor.delegate = self
         interactor.fetchData()
+        title = "Space X"
     }
 
     private func addComponents() {
@@ -80,7 +83,7 @@ extension HomeViewController: UITableViewDataSource {
 
         switch item {
         case .companyInfo(let viewModel): (cell as? CompanyInfoCell)?.configure(with: viewModel)
-        case .launch(let viewModel):  (cell as? LaunchCell)?.configure(with: viewModel)
+        case .launch(let viewModel):  (cell as? LaunchCell)?.configure(with: viewModel, imageDownloader: imageDownloader)
         }
 
         return cell
@@ -107,9 +110,30 @@ extension HomeViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         UITableView.automaticDimension
     }
+
+    func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        guard section == 1 else { return }
+        interactor.fetchNextPage()
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        interactor.showActionForItem(at: indexPath)
+    }
 }
 
 extension HomeViewController: HomeInteractorDelegate {
+    func interactor(_ interactor: HomeInteractor, wantsToShowWebsiteWithURL url: URL) {
+        let safariViewController = SFSafariViewController(url: url)
+        present(safariViewController, animated: true, completion: nil)
+    }
+
+    func interactor(_ interactor: HomeInteractor, wantsToShowError error: Error) {
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alertController.addAction(.init(title: "Ok", style: .cancel, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
     func interactor(_ interactor: HomeInteractor, didUpdateViewModel viewModel: HomeViewModel) {
         tableView.reloadData()
     }
