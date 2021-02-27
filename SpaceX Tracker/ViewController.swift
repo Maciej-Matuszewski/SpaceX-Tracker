4038c41d925d3335c9b2406ccf8b568fb1b25564
Maciej Matuszewski <maciej.matuszewski@me.com>
2021-02-27 11:53:16 +0000

    Added basic TableView in ViewController

--- SpaceX Tracker/ViewController.swift
+++ SpaceX Tracker/ViewController.swift
@@ -9,9 +9,55 @@ import UIKit
 import Style
 
 class ViewController: UIViewController {
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .style(.primaryBackground)
+
+        addComponents()
+        layoutComponents()
+    }
+
+    private func addComponents() {
+        view.addSubview(tableView)
+    }
+
+    private func layoutComponents() {
+        NSLayoutConstraint.activate(
+            [
+                tableView.topAnchor.constraint(equalTo: view.topAnchor),
+                tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ]
+        )
+
+        tableView.contentInset.bottom = view.safeAreaInsets.bottom
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Section: \(indexPath.section), Row: \(indexPath.row)"
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate { }
